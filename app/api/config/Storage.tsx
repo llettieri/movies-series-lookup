7e5c4e6846662bd1ca57e3ce183845b26d19eaa4
llettieri <lorenzolettieri@sunrise.ch>
2023-08-21 21:29:31 +0200

    new endpoints for tvshows

---
+++ app/api/config/Storage.tsx
@@ -0,0 +1,17 @@
+import { StorageKeys } from '@/app/api/config/StorageKeys';
+
+const getKey = (key: StorageKeys): string => `movies-series::${key}`;
+
+const localSaveItem = <T,>(key: StorageKeys, item: T): void =>
+    localStorage.setItem(getKey(key), JSON.stringify(item));
+
+const localGetItem = <T,>(key: StorageKeys): T =>
+    JSON.parse(localStorage.getItem(getKey(key)) ?? '');
+
+const sessionSaveItem = <T,>(key: StorageKeys, item: T): void =>
+    sessionStorage.setItem(getKey(key), JSON.stringify(item));
+
+const sessionGetItem = <T,>(key: StorageKeys): T =>
+    JSON.parse(sessionStorage.getItem(getKey(key)) ?? '');
+
+export { localSaveItem, localGetItem, sessionGetItem, sessionSaveItem };
--- app/api/hooks/useMediaLists.tsx
+++ app/api/hooks/useMediaLists.tsx
@@ -1,27 +1,31 @@
 import { api } from '@/app/api/config/AxiosInstance';
 import { routes } from '@/app/api/config/routes';
+import { localGetItem } from '@/app/api/config/Storage';
+import { StorageKeys } from '@/app/api/config/StorageKeys';
 import { parseMovieDto, parseTVShowDto } from '@/app/api/services/ParseService';
 import { ListDto } from '@/models/dto/ListDto';
 import { MovieDto } from '@/models/dto/MovieDto';
 import { TVShowDto } from '@/models/dto/TVShowDto';
 import { Media } from '@/models/Media';
 import { TVShow } from '@/models/TVShow';
+import dayjs from 'dayjs';
 import useSWR from 'swr';
 import { parseTemplate } from 'url-template';
 
 interface UseMovieValues {
-    latestIsLoading: boolean;
+    airingTodayShows: TVShow[];
     latestMovies: Media[];
-    popularMoviesIsLoading: boolean;
+    loadingAiringToday: boolean;
+    loadingLatestMovies: boolean;
+    loadingPopularMovies: boolean;
+    loadingPopularShows: boolean;
     popularMovies: Media[];
     popularShows: TVShow[];
-    popularShowsIsLoading: boolean;
-    airingTodayShows: TVShow[];
-    airingTodayIsLoading: boolean;
 }
 
 export const useMediaLists = (): UseMovieValues => {
-    const { data: latestMovies, isLoading: latestIsLoading } = useSWR(
+    const today = dayjs().format('YYYY-MM-DD');
+    const { data: latestMovies, isLoading: loadingLatestMovies } = useSWR(
         parseTemplate(routes.movies.nowPlaying).expand({ page: 1 }),
         (url) =>
             api()
@@ -30,7 +34,7 @@ export const useMediaLists = (): UseMovieValues => {
         { refreshInterval: 86400 },
     );
 
-    const { data: popularMovies, isLoading: popularMoviesIsLoading } = useSWR(
+    const { data: popularMovies, isLoading: loadingPopularMovies } = useSWR(
         parseTemplate(routes.movies.popular).expand({ page: 1 }),
         (url) =>
             api()
@@ -39,8 +43,11 @@ export const useMediaLists = (): UseMovieValues => {
         { refreshInterval: 86400 },
     );
 
-    const { data: popularShows, isLoading: popularShowsIsLoading } = useSWR(
-        parseTemplate(routes.tv.popular).expand({ page: 1 }),
+    const { data: popularShows, isLoading: loadingPopularShows } = useSWR(
+        parseTemplate(routes.tv.discover).expand({
+            watch_region: localGetItem(StorageKeys.REGION),
+            with_watch_monetization_types: 'flatrate|free|ads|rent|buy',
+        }),
         (url) =>
             api()
                 .get<ListDto<TVShowDto>>(url)
@@ -48,8 +55,13 @@ export const useMediaLists = (): UseMovieValues => {
         { refreshInterval: 86400 },
     );
 
-    const { data: airingTodayShows, isLoading: airingTodayIsLoading } = useSWR(
-        parseTemplate(routes.tv.airingToday).expand({}),
+    const { data: airingTodayShows, isLoading: loadingAiringToday } = useSWR(
+        parseTemplate(routes.tv.discover).expand({
+            watch_region: localGetItem(StorageKeys.REGION),
+            with_watch_monetization_types: 'flatrate|free|ads|rent|buy',
+            'air_date.gte': today,
+            'air_date.lte': today,
+        }),
         (url) =>
             api()
                 .get<ListDto<TVShowDto>>(url)
@@ -58,13 +70,13 @@ export const useMediaLists = (): UseMovieValues => {
     );
 
     return {
-        latestIsLoading,
+        airingTodayShows: airingTodayShows ?? [],
         latestMovies: latestMovies ?? [],
-        popularMoviesIsLoading,
+        loadingAiringToday,
+        loadingLatestMovies,
+        loadingPopularMovies,
+        loadingPopularShows: loadingPopularShows,
         popularMovies: popularMovies ?? [],
         popularShows: popularShows ?? [],
-        popularShowsIsLoading,
-        airingTodayShows: airingTodayShows ?? [],
-        airingTodayIsLoading,
     };
 };
