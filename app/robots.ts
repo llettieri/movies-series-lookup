7445ee34ffe8a75ebadc2b339523e9bc6ea22d26
llettieri <lorenzolettieri@sunrise.ch>
2024-12-28 16:54:53 +0100

    ai bots blocking

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,33 @@ import { MetadataRoute } from 'next';
 
 export default function Robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/private/',
+            },
+            {
+                userAgent: [
+                    'AI2Bot',
+                    'Ai2Bot-Dolma',
+                    'anthropic-ai',
+                    'ChatGPT-User',
+                    'Claude-Web',
+                    'ClaudeBot',
+                    'cohere-ai',
+                    'Diffbot',
+                    'DuckAssistBot',
+                    'GPTBot',
+                    'Meta-ExternalAgent',
+                    'Meta-ExternalFetcher',
+                    'OAI-SearchBot',
+                    'PerplexityBot',
+                    'PanguBot',
+                ],
+                disallow: '/',
+            },
+        ],
         sitemap: 'https://movies-series.lore-le.ch/sitemap.xml',
     };
 }
