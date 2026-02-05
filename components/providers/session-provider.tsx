4450fcf392eb98daf8ed7fdb83a6adb14cf9b005
llettieri <lorenzolettieri@sunrise.ch>
2026-02-05 21:43:53 +0100

    task: improved session handling

--- components/providers/session-provider.tsx
+++ components/providers/session-provider.tsx
@@ -4,6 +4,9 @@ import { apiRoutes } from '@/config/api-routes';
 import { TMDBApi } from '@/services/api';
 import { createSession } from '@/services/session-service';
 import { ReactNode, useEffect } from 'react';
+import { HttpStatusCode } from 'axios';
+
+const FALLBACK_COUNTRY_CODE = 'US';
 
 interface GeoDataDto {
     region: string;
@@ -14,14 +17,22 @@ interface GeoDataDto {
 export const SessionProvider = (): ReactNode => {
     useEffect(() => {
         TMDBApi.get<GeoDataDto>(apiRoutes.country)
-            .then((r) => createSession(r.data.countryCode))
+            .then((r) => {
+                let countryCode = FALLBACK_COUNTRY_CODE;
+
+                if (r?.status === HttpStatusCode.Ok) {
+                    countryCode = r.data.countryCode;
+                }
+
+                return createSession(countryCode);
+            })
             .catch((e) => {
                 // eslint-disable-next-line
                 console.error(
                     'Something went wrong with checking your location: %s',
                     e,
                 );
-                return createSession('US');
+                return createSession(FALLBACK_COUNTRY_CODE);
             });
     }, []);
 
